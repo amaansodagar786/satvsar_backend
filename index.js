f3956ee3752c94743bacc2d09cc61462adf1ebe2
Amaan Sodagar <sodagaramaan786@gmail.com>
2025-11-19 14:55:37 +0530

    commit

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ cron.schedule("0 0 1 1 *", async () => {
 
 // Basic Route
 app.get('/', (req, res) => {
-  res.send('Hello World from Jass Inventory BackendD !');
+  res.send('Hello World from Satvsar Inventory Backend !');
 });
 
 // Server
